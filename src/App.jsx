096470c14fc3f545143e85791ddbb5fb2147fd7d
valeriya.laktionova <valeriya.laktionova@student.ehu.lt>
2025-04-05 11:18:05 +0300

    feat:hw-1 done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ItemList } from './components/ItemList';
+
+const items = ["1", "2", "3", "4"];
+
+function App() {
+
+  return (
+    <>
+      <ItemList items={items} />
+    </>
+  )
+}
+
+export default App
